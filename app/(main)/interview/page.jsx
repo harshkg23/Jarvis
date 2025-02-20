b06a8ee79211f86fdc36564ba949473fab1d5d7b
Harsh Kumar Gupta <harshkumargupta2023@gmail.com>
2025-02-20 23:43:29 +0530

    feat: implement resume and interview preparation pages; add layout components and performance tracking features

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { getAssessments } from "@/actions/interview";
+import StatsCard from "./_components/Stats-cards";
+import PerformanceChart from "./_components/Performance-chart";
+import QuizList from "./_components/QuizList";
 
-function InterviewPage() {
+
+export default async function InterviewPrepPage() {
+  const assessments = await getAssessments();
+
   return (
     <div>
-      
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold gradient-title">
+          Interview Preparation
+        </h1>
+      </div>
+      <div className="space-y-6">
+        <StatsCard assessments={assessments} />
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
     </div>
-  )
+  );
 }
-
-export default InterviewPage
